a16e26c22999a190f55992d337aaa15bd280fc44
SAIKUMARREDDY POCHIREDDYGARI <vishukings@gmail.com>
2020-05-29 21:09:18 +0530

    initial draft

---
+++ bubblesort.py
@@ -0,0 +1,17 @@
+def bubbleSort(arr):
+    """
+    This function is used to sort the array adcending order
+    """
+    #Test array = [ 3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
+
+    for i in range(0,len(arr)):
+        for j in range (0, len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j] , arr[j+1] = arr[j+1] , arr[j]
+
+    return (arr)
+
+if __name__ == "__main__":
+    arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
+    print(bubbleSort(arr))
+
